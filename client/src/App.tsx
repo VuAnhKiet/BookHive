b27fbab14851b0bf395371833d6daf0b625ca96c
Kiet <honeywork175@gmail.com>
2025-04-02 20:58:13 +0700

    Initial stage

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './pages/Home';
+import Header from './pages/Header';
+import Footer from './pages/Footer';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
