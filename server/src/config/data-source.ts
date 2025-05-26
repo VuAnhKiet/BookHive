9f393c72f5761fc0d6251ce3556c00ee3f806b14
Kiet <honeywork175@gmail.com>
2025-05-26 19:07:03 +0700

    config database connection

---
+++ server/src/config/data-source.ts
@@ -0,0 +1,20 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); 
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/../entities/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../migrations/*.ts'],
+  synchronize: false,
+  logging: true,
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
